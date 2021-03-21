445d57a703cb4677efcdae0f6936b689ce659723
BenTho-Uni <benjamin.thomitzni@iwr.uni-heidelberg.de>
2021-03-21 15:24:34 +0100

    Added system agnostic path to sphinx docu

--- src/ssc_course_team4/io_layer.py
+++ src/ssc_course_team4/io_layer.py
@@ -16,12 +16,28 @@ def read_in_np(filepath):
 
 
 def euclid_dis_plot(filepath_out, data):
+    """Plots the Euclidean Distance as a plot of the different axis
+
+    Args:
+        filepath_out (string): The realtive output file filepath.
+        data (numpy array): The data holding object.
+
+    Returns:
+        """
     plt.bar(range(0, len(data)), data)
     plt.savefig(filepath_out+'euclid_dis.pdf')
     return
 
 
 def efield_plot(filepath_out, data):
+    """Plots the electric field as a plot of the different axis
+
+    Args:
+        filepath_out (string): The realtive output file filepath.
+        data (numpy array): The data holding object.
+
+    Returns:
+    """
     plt.plot(data['time'], data['x'])
     plt.plot(data['time'], data['y'])
     plt.plot(data['time'], data['z'])
@@ -31,6 +47,12 @@ def efield_plot(filepath_out, data):
 
 
 def efield_fft_plot(filepath_out, data):
+    """Plots the Fourier-transformed electric efield_df.
+
+    Args:
+        filepath_out (string): The realtive output file filepath.
+        data (numpy array): The data holding object.
+    Returns:"""
     efield_np_fft_data = np.fft.rfft(data[1])
     efield_np_fft_freq = np.fft.rfftfreq(len(data[0]))
     plt.plot(efield_np_fft_freq, abs(efield_np_fft_data**2))
