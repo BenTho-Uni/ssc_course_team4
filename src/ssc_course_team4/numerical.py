49af61bc13a5c40e0521c560c7555bfcbeaae615
Nils Oberhof <nils.oberhof@iwr.uni-heidelberg.de>
2021-03-30 11:22:52 +0200

    zwischencommit3

--- src/ssc_course_team4/numerical.py
+++ src/ssc_course_team4/numerical.py
@@ -1,78 +1,4 @@
-<<<<<<< HEAD
-# Numerical Analysis
-import io_layer as io
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sn
 
-
-#data = io.read_in_df(filedir, filename)
-#alternatively defining data once and then only read in data for the other functions?
-
-def expect_plot(filedir, filename, threshv):
-    """ reads in dataframe file, drops data with variance below threshold and plots relevant data
-
-    Args:   filedir: directory of the dataframe file
-            filename: name of the file
-            thresv: desired threshold value
-
-    Return: relplots of the relevant data
-    """
-    df_expec = io.read_in_df(filedir, filename)
-    df_expec2=df_expec.drop(df_expec.var()[df_expec.var() < threshv].index.values, axis=1)
-    sn.relplot(x='time', y='value', hue='variable', data=pd.melt(df_expec2, ['time']), kind='line')
-    plt.show()
-    return
-
-#example test run:
-#expect_plot('~/ssc_course_team4/data/', 'expec.t', 0.00001)
-
-def npop_plot(filedir, filename, threshv):
-    """ reads in dataframe file, drops data with variance below threshold and plots relevant data
-
-    Args:   filedir: directory of the dataframe file
-            filename: name of the file
-            thresv: desired threshold value
-
-    Return: relplots of the relevant data
-    """
-
-    df_npop = io.read_in_df(filedir, filename)
-    df_npop2 = df_npop.drop(df_npop.var()[df_npop.var() < threshv].index.values, axis=1)
-    sn.lineplot(x='time', y='value', hue='variable', data=pd.melt(df_npop2, ['time']))
-    plt.show()
-    return
-
-#example test run:
-#npop_plot('~/ssc_course_team4/data/', 'npop.t', 0.00001)
-
-def npop_corr(filedir, filename, threshv):
-    """ reads in dataframe file, drops data with variance below threshold and redundant entries,
-        sorts for absolute correlation values and prints it to npop_corr.cvs
-
-    Args:   filedir: directory of the dataframe file
-            filename: name of the file
-            thresv: desired threshold value
-
-    Return: npop_corr.cvs file with sorted, non-redundant correlation values
-    """
-    df_npop = io.read_in_df(filedir, filename)
-    df_npop2_short = df_npop.drop(df_npop.var()[df_npop.var() < threshv].index.values, axis=1)
-    value_list = set()
-    column_names = df_npop2_short.columns
-    for i in range(0, df_npop2_short.shape[1]):
-        for j in range(0, i+1):
-            value_list.add((column_names[i], column_names[j]))
-    final_correlation = df_npop2_short.corr().unstack()
-    final_correlation = final_correlation.drop(labels=value_list).sort_values(ascending=False, key=lambda col: col.abs())
-    with open('npop_corr.cvs', 'w') as f:
-        print(final_correlation, file=f)
-    return
-
-#example test run:
-#npop_corr('~/ssc_course_team4/data/', 'npop.t', 0.000001)
-=======
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -196,4 +122,3 @@ def plot_autocorr_fft(filepath_in, filepath_out):
     plt.show()
 
     return
->>>>>>> origin/ben
