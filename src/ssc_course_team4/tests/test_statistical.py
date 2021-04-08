3990a545c80856d8b78c2e4f5a30fd47fc8cc137
Nils Oberhof <nils.oberhof@iwr.uni-heidelberg.de>
2021-04-08 11:03:21 +0200

    npop_corr test works now

--- src/ssc_course_team4/tests/test_statistical.py
+++ src/ssc_course_team4/tests/test_statistical.py
@@ -1,6 +1,8 @@
 import numpy as np
 import ssc_course_team4.statistical as st
 import ssc_course_team4.io_layer as io
+import filecmp
+import os
 
 
 def test_euclid_dis():
@@ -15,3 +17,22 @@ def test_euclid_dis():
 
     # compare both results
     assert np.allclose(test_result, ref_result, atol=1e-8)
+
+
+def test_npop_corr():
+    """ Test npop_corr through statistical.npop_corr().
+    """
+
+    # calling fuction to test with reference data
+    filepath = "ssc_course_team4/tests/data/ref_npop_corr.csv"
+    test_results = st.npop_corr(io.read_in_df(filepath), 1e-5)
+    # print to file to compare it
+    test_results.to_csv('testfile.csv')
+    # reference results
+    filepath = "ssc_course_team4/tests/data/ref_npop_corr_results.csv"
+
+    # compare results
+    assert filecmp.cmp('ssc_course_team4/tests/data/ref_npop_corr_results.csv',
+                       'testfile.csv', shallow=True)
+    # get rid of unnecessary amount of files
+    os.remove('testfile.csv')
